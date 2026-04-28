2de42adc232cbbe46e7fe89f0da4e1826a38bef3
marouazouidi <zouidimaroua@gmail.com>
2026-04-28 12:26:45 +0100

    make fake Categories and laravel debugbar/telescope

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+
+
+
+Schedule::command('telescope:prune --hours=48')->daily();
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
